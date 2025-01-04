3074bae019d5809b136b35a079e507f311263c5b
hyunw00 <khwland090@gmail.com>
2025-01-04 21:44:51 +0900

    ✨ Feature: swiper 공통 컴포넌트 구현

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,70 +1,76 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: 
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDdhZWVjNzc4MDEzMmNkMzRlODQ1NzU4NzNhMmY5NCIsIm5iZiI6MTczMzE5NzQ2OS4xODIwMDAyLCJzdWIiOiI2NzRlN2U5ZGFhODQ0ZGM2ZWU5NDUzODEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.PaxpoOBPaH4s3kE5c3dXJe3pY0p0TCXzBAavEWHMbT4'
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
   },
 });
 
 // movie- trending
-export const getTrendingMovie = async (timeWindow: 'day' | 'week') => {
-  try{
-    const {data: {results}} = await axiosInstance.get(`/trending/movie/${timeWindow}`)
+export const getTrendingMovie = async (timeWindow: "day" | "week") => {
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get(`/trending/movie/${timeWindow}`);
     return results;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching trendingMovie:", error);
     throw error;
   }
-}
-
-
+};
 
 // 영화- nowplaying
 export const getMovieNowPlaying = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/movie/now_playing");
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/movie/now_playing");
     return results;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching NowPlaying:", error);
     throw error;
   }
-}
+};
 
 // 영화 -popular
 export const getMoviePopular = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/movie/popular");
-    const sortedPopularity = results.sort((a:MovieItem, b:MovieItem) => b.popularity - a.popularity);
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/movie/popular");
+    const sortedPopularity = results.sort((a: MovieItem, b: MovieItem) => b.popularity - a.popularity);
     return sortedPopularity;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching popular:", error);
     throw error;
   }
-}
+};
 
 //영화 - Upcoming
 export const getMovieUpcoming = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/movie/upcoming",);
-    const sortedRelease = results.sort((a: MovieItem, b:MovieItem) => {
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/movie/upcoming");
+    const sortedRelease = results.sort((a: MovieItem, b: MovieItem) => {
       return new Date(b.release_date).getTime() - new Date(a.release_date).getTime();
     });
 
     return sortedRelease;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching Upcoming:", error);
     throw error;
   }
-}
+};
 
 // 영화 - 이미지 가져오기
 export const getMovieStills = async (movieId: number) => {
-  try{
+  try {
     const response = await axiosInstance.get(`/movie/${movieId}/images`);
-    return response.data.backdrops; 
-  } catch (error){
+    return response.data.backdrops;
+  } catch (error) {
     console.error("Error fetching stills:", error);
     throw error;
   }
@@ -74,7 +80,7 @@ export const getMovieStills = async (movieId: number) => {
 export const getGenreMap = async () => {
   try {
     const response = await axiosInstance.get(`/genre/movie/list`, {
-      params: { language: "en-US" }, 
+      params: { language: "en-US" },
     });
     const genres = response.data.genres;
     return genres.reduce((map: Record<number, string>, genre: { id: number; name: string }) => {
@@ -88,78 +94,83 @@ export const getGenreMap = async () => {
 };
 
 // 영화 - 디테일 정보
-export const getMovieDetails = async (id:number): Promise<MovieItem> => {
-  try{
+export const getMovieDetails = async (id: number): Promise<MovieItem> => {
+  try {
     const response = await axiosInstance.get(`/movie/${id}`, {
-      params: { language: "en-US" }, 
+      params: { language: "en-US" },
     });
     const details = response.data;
     return details;
-  } catch (error){
+  } catch (error) {
     console.error("Error API details:", error);
     throw error;
   }
+};
+
+{
+  /* TV Series */
 }
-
-
-
-{/* TV Series */}
 
 // Tv- Airing Today
 export const getTvAiringToday = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/tv/airing_today");
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/tv/airing_today");
     return results;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching Airing Today:", error);
     throw error;
   }
-}
+};
 
 // Tv - Popular
 export const getTVPopular = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/tv/popular");
-    const sortedPopularity = results.sort((a:TvItem, b:TvItem) => b.popularity - a.popularity);
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/tv/popular");
+    const sortedPopularity = results.sort((a: TvItem, b: TvItem) => b.popularity - a.popularity);
     return sortedPopularity;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching TvPopular:", error);
     throw error;
   }
-}
+};
 
 // Tv - Top Rated
 export const getTvTopRated = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/tv/top_rated",);
-    const sortedVoteAvg = results.sort((a:TvItem, b:TvItem) => b.vote_average - a.vote_average);
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/tv/top_rated");
+    const sortedVoteAvg = results.sort((a: TvItem, b: TvItem) => b.vote_average - a.vote_average);
     return sortedVoteAvg;
-
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching Top Rated:", error);
     throw error;
   }
-}
+};
 
 // Tv - On The Air
 export const getTvOnTheAir = async () => {
-  try{
-    const {data: {results}} = await axiosInstance.get("/tv/on_the_air",);
+  try {
+    const {
+      data: { results },
+    } = await axiosInstance.get("/tv/on_the_air");
     return results;
-  } catch (error){
+  } catch (error) {
     console.error("Error fetching On The Air:", error);
     throw error;
   }
-}
-
-
+};
 
 // TV - 이미지 가져오기
 export const getTvStills = async (series_id: number) => {
-  try{
+  try {
     const response = await axiosInstance.get(`/tv/${series_id}/images`);
-    return response.data.backdrops; 
-  } catch (error){
+    return response.data.backdrops;
+  } catch (error) {
     console.error("Error fetching Tv stills:", error);
     throw error;
   }
@@ -169,7 +180,7 @@ export const getTvStills = async (series_id: number) => {
 export const getTVGenres = async () => {
   try {
     const response = await axiosInstance.get(`/genre/tv/list`, {
-      params: { language: "en-US" }, 
+      params: { language: "en-US" },
     });
     const genres = response.data.genres;
     return genres.reduce((map: Record<number, string>, genre: { id: number; name: string }) => {
@@ -182,19 +193,18 @@ export const getTVGenres = async () => {
   }
 };
 
-
 // 영화 검색
-export const searchMovie = async(query: string) => {
-  if(!query) return [];
-  try{
-    const response = await axiosInstance.get(`/search/movie`,{
-      params:{
-        query
+export const searchMovie = async (query: string) => {
+  if (!query) return [];
+  try {
+    const response = await axiosInstance.get(`/search/movie`, {
+      params: {
+        query,
       },
     });
     return response.data.results;
-  }catch (error) {
+  } catch (error) {
     console.error("Error fetching search results:", error);
     return [];
   }
-}
+};
--- src/components/recommend/RecommendCard.tsx
+++ src/components/recommend/RecommendCard.tsx
@@ -1,9 +1,74 @@
-import RecommendItem from "./RecommendItem";
+import { useEffect, useState } from "react";
+import { getGenreMap, getMovieStills } from "../../api/axios";
+import { Link } from "react-router";
+import playIcon from "../../assets/Play_icon.svg";
 
-export default function RecommendCard({movie}:RecommendProps) {
+export default function RecommendCard({ movie }: RecommendProps) {
+  const [backdropPath, setBackdropPath] = useState("");
+  const [firstGenre, setFirstGenre] = useState<string>("");
+  const [secondGenre, setSecondGenre] = useState<string>("");
+
+  useEffect(() => {
+    const fetchMovieStills = async () => {
+      try {
+        const backdrops = await getMovieStills(movie.id);
+        if (backdrops && backdrops.length > 0) {
+          setBackdropPath(backdrops[5].file_path);
+        } else {
+          console.error("No backdrops available.");
+        }
+
+        const genreMap = await getGenreMap();
+        const genres = movie.genre_ids.slice(0, 2).map((id: number) => genreMap[id] || "Unknown");
+
+        setFirstGenre(genres[0] || "N/A");
+        setSecondGenre(genres[1] || "N/A");
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    };
+    fetchMovieStills();
+  }, [movie.id, movie.genre_ids]);
+
   return (
-    <div>
-      <RecommendItem movie={movie}/>
-    </div>
-  )
+    <Link to={`/movie/${movie.id}`}>
+      <div className="w-[1030px] h-[600px] rounded-[30px] relative overflow-hidden mt-10">
+        {/* 그라데이션 오버레이 */}
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#000000] via-[#1E1E1E]/30 to-transparent z-10" />
+        {/* 영화 정보 */}
+        <div className="absolute z-20 text-white font-noto bottom-[60px]">
+          {/* 영화 장르 */}
+          <div className="flex flex-row items-center gap-2 ml-10 text-[13px]">
+            <p className="bg-black/60 px-4 py-1 rounded-full">{firstGenre}</p>
+            <p className="bg-black/60 px-4 py-1 rounded-full">{secondGenre}</p>
+          </div>
+
+          {/* 영화 타이툴 */}
+          <p
+            className={`font-extrabold ml-10 w-[500px] mt-4
+          ${movie.title.length > 20 ? "text-[30px] leading-tight" : "text-[35px]"}`}
+          >
+            {movie.title}
+          </p>
+
+          {/* 영화 개요 */}
+          <p className={`text-[13px] w-[400px] mt-4 ml-10 text-white/70`}>
+            {movie.overview.length < 240 ? movie.overview : `${movie.overview.slice(0, 240)}...`}
+          </p>
+
+          <div className="relative flex flex-row items-center mt-4 font-pretendard font-semiBold ml-10">
+            <img className="absolute ml-1.5 w- h-5" src={playIcon} alt="재생하기" />
+            <p className="px-4 py-1 bg-white text-black rounded-full text-[13px]">ㅤWatch </p>
+          </div>
+        </div>
+
+        {/* 영화 스틸이미지 */}
+        <img
+          className="w-full h-full rounded-[30px] object-cover"
+          src={`https://image.tmdb.org/t/p/original${backdropPath}`}
+          alt={movie.title}
+        />
+      </div>
+    </Link>
+  );
 }
