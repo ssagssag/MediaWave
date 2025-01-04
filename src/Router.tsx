23e371f4293f85b5f13065a3ba195d054e17c876
wl990@naver.com <wl990@naver.com>
2025-01-04 13:58:16 +0900

    ✨ 기초 라우팅 페이지 + 라우팅 작업 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,33 @@
-import DetailMovie from "./pages/DetailMovie";
 import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./layout/RootLayout";
+import Login from "./pages/login/Login";
+import Search from "./pages/search/Search";
+import Movie from "./pages/movie/Movie";
+import Tv from "./pages/tv/Tv";
+import Genre from "./pages/genre/Genre";
+import DetailTv from "./pages/details/DetailTv";
+import Animation from "./pages/animation/Animation";
+import DetailMovie from "./pages/details/DetailMovie";
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/animation" element={<Animation />} />
+        <Route path="/genre" element={<Genre />} />
+        <Route path="/details/tv/:id" element={<DetailTv />} />
+        <Route path="/details/movie/:id" element={<DetailMovie />} />
       </Route>
       <Route path="/movie/:id" element={<DetailMovie />} />
 
-      <Route path="*" element={<NotFound />}/>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
--- src/pages/movie/Movie.tsx
+++ src/pages/movie/Movie.tsx
@@ -1,20 +1,22 @@
-import Movies from "../components/Movies";
-import Recommend from "../components/recommend/Recommend";
-import Trending from "../components/TrendingMovie";
+import Movies from "../../components/Movies";
+import Recommend from "../../components/recommend/Recommend";
+import TrendingMovie from "../../components/TrendingMovie";
 
 export default function Movie() {
   return (
-    <main className={`
-      flex flex-col items-center justify-center`}>
+    <main
+      className={`
+      flex flex-col items-center justify-center`}
+    >
       <div className="w-[1440px] flex flex-row justify-between gap-7 mt-16 px-10">
         <aside className="w-[400px] h-[600px] mt-11 overflow-hidden">
-          <Trending />
+          <TrendingMovie />
         </aside>
         <section className="w-[1030px] h-full">
           <Recommend />
         </section>
       </div>
-        <Movies />
+      <Movies />
     </main>
-  )
+  );
 }
