063669f5096e68098e173a8400e4ee0634005b48
wl990@naver.com <wl990@naver.com>
2025-01-04 12:48:13 +0900

    🩹 package-lock.json파일 ignore 처리

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from './App.tsx'
-import './styles/index.css'
+import App from "./App.tsx";
+import "./styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
