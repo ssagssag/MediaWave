09ed13b6f36637829a355689d6b7840335f897a2
wl990@naver.com <wl990@naver.com>
2025-01-04 16:03:44 +0900

    📝 spinner UI 컴포넌트 생성(components -> spinner -> Spinner.tsx), skeleton UI 애니메이션 생성(skeleton 을 클래스명으로 주면 적용됨)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pretendard: ['Pretendard', 'sans-serif'],
-        title: ['PartialSansKR-Regular', 'ui-sans-serif', 'system-ui'],
-        noto: ['"Noto Sans"', 'sans-serif'],
+        pretendard: ["Pretendard", "sans-serif"],
+        title: ["PartialSansKR-Regular", "ui-sans-serif", "system-ui"],
+        noto: ['"Noto Sans"', "sans-serif"],
       },
       fontWeight: {
         regular: 400,
@@ -16,23 +16,37 @@ export default {
         extraBold: 800,
       },
       boxShadow: {
-        'default': '0px 8px 18px 0px rgba(168, 178, 198, 0.45)',
-        'custom-light': '0 10px 10px rgba(255, 255, 255, 0.1)',
-        'custom-heavy': '0 10px 10px rgba(0, 0, 0, 0.3)',
+        default: "0px 8px 18px 0px rgba(168, 178, 198, 0.45)",
+        "custom-light": "0 10px 10px rgba(255, 255, 255, 0.1)",
+        "custom-heavy": "0 10px 10px rgba(0, 0, 0, 0.3)",
       },
       colors: {
-        gray: '#7d848d',
-        black: '#2e2e2e',
-        graylight: '#D7D7D7',
-        input: '#FEFEFE',
+        gray: "#7d848d",
+        black: "#2e2e2e",
+        graylight: "#D7D7D7",
+        input: "#FEFEFE",
       },
       fontSize: {
-        clamp: 'clamp(30px, 4vw, 50px)',
+        clamp: "clamp(30px, 4vw, 50px)",
       },
+      // 스켈레톤
+      animation: {
+        shimmer: "shimmer 1.5s infinite linear",
+      },
+      keyframes: {
+        shimmer: {
+          "0%": { backgroundPosition: "200%" },
+          "100%": { backgroundPosition: "-200%" },
+        },
+      },
+      backgroundImage: {
+        "gradient-custom": "linear-gradient(to right, #D9D9D9 0%, #EDEEF1 50%, #D9D9D9 100%)",
+      },
+      backgroundSize: {
+        custom: "300% 100%",
+      },
+      // 스켈레톤
     },
-    
   },
-  plugins: [
-    require('tailwind-scrollbar-hide'),
-  ],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
