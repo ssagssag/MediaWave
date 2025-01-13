566cba0ce21c82fd60b1aefcdf44346ceecfbac2
swallowedB <musamea99@gmail.com>
2025-01-13 23:02:56 +0900

    💄 Style: 검색페이지 - 오른쪽 MediaCard 짤리는 영역 CSS 수정

--- src/pages/search/components/MediaCard.tsx
+++ src/pages/search/components/MediaCard.tsx
@@ -5,7 +5,7 @@ export default function MediaCard({ media }: MediaCardProps) {
   return (
     <div className={`
       grid grid-cols-1 sm:grid-cols-2 gap-y-14 gap-x-14 
-      w-full max-w-screen-lg overflow-hidden mt-10
+      w-full max-w-screen-lg mt-10
       `}>
       {media.map((item) => 
         <MediaCardItem key={item.id} item={item}  />
--- src/pages/search/components/MediaCardItem.tsx
+++ src/pages/search/components/MediaCardItem.tsx
@@ -1,19 +1,25 @@
 import { useNavigate } from "react-router";
 
 export default function MediaCardItem({ item }: MediaCardItemProps) {
-  const title = item.media_type === "movie" ? item.title || "Untitled Movie"  : item.name|| "Untitled TV";
+  const title = 
+    item.media_type === "movie" 
+      ? item.title || "Untitled Movie"  
+      : item.name|| "Untitled TV";
   const isLongTitle = title.length > 8;
   const nav = useNavigate();
 
   return (
     <div
-    onClick={() => item.media_type === "movie" ? nav(`/movie/${item.id}`) : nav(`/tv/${item.id}`) } 
+    onClick={() => 
+      item.media_type === "movie" 
+        ? nav(`/movie/${item.id}`) 
+        : nav(`/tv/${item.id}`) } 
     className="flex flex-row items-center gap-6 w-full max-w-full px-10 cursor-pointer">
       {/* 영화 poster 영역 */}
       <img
         src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
         alt={title}
-        className="bg-main-300 w-[150px] h-[220px] rounded-2xl flex-shrink-0"
+        className="bg-main-300 w-[150px] h-[220px] rounded-xl flex-shrink-0"
       />
       {/* 영화 info 영역 */}
       <div
@@ -33,7 +39,9 @@ export default function MediaCardItem({ item }: MediaCardItemProps) {
 
         <div className="overflow-hidden">
           <p className="text-sm text-white/70 line-clamp-4 ">
-            {item.overview.length < 300 ? item.overview : item.overview.slice(0, 220) + "..."}
+            {item.overview.length < 300 
+              ? item.overview 
+              : item.overview.slice(0, 220) + "..."}
           </p>
         </div>
 
